2d6bd8c3e7a5d1ef80c6409efa5fd7a099ed0aae
taccisum <liaojinfeng@deepexi.com>
2019-12-14 18:38:41 +0800

    feat: add foo-service module

---
+++ foo-service/src/main/java/com/github/taccisum/swagger/aggregation/sample/app/FooApplication.java
@@ -0,0 +1,17 @@
+package com.github.taccisum.swagger.aggregation.sample.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author taccisum - liaojinfeng@deepexi.com
+ * @since 2019-12-14
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FooApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FooApplication.class, args);
+    }
+}
